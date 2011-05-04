76c002a8405346114f0b101e251e53c7436d7eed
Woodyman <woodyman.dc@gmail.com>
2011-05-03 22:32:49 -0400

    fix my errors, and use preference over layout

--- Android.mk
+++ Android.mk
@@ -8,6 +8,3 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := DroidConceptsSettings
 
 include $(BUILD_PACKAGE)
-
-# Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ src/com/droidconcepts/settings/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.droidconcepts.settings.activities;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+import com.droidconcepts.settings.R;
+
+
+public class MainActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.dcs);
+    }
+}
