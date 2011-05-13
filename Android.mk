904453bc62956100e10541c47df741a8f5441551
Woodyman <woodyman.dc@gmail.com>
2011-05-12 23:36:27 -0400

    have DCS run as "system" thank you CVPCS for pointing out my error :)

--- Android.mk
+++ Android.mk
@@ -6,5 +6,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := DroidConceptsSettings
+LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
