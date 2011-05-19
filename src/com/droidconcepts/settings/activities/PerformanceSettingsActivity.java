e4e4ef7d72b8bc70ce22c431a0d920741c875e81
Woodyman <woodyman.dc@gmail.com>
2011-05-18 22:57:09 -0400

    add configurable davlik cache

--- src/com/droidconcepts/settings/activities/PerformanceSettingsActivity.java
+++ src/com/droidconcepts/settings/activities/PerformanceSettingsActivity.java
@@ -4,10 +4,12 @@ import com.droidconcepts.settings.R;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.util.Log;
 
 import java.io.File;
 
@@ -15,14 +17,19 @@ import java.io.File;
  * Performance Settings
  */
 
-public class PerformanceSettingsActivity extends PreferenceActivity {
+public class PerformanceSettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
     private static final String LOCK_HOME_PREF = "pref_lock_home";
     private static final int LOCK_HOME_DEFAULT = 0;
     private static final String LOCK_MMS_PREF = "pref_lock_mms";
     private static final int LOCK_MMS_DEFAULT = 1;
+    private static final String HEAPSIZE_PREF = "pref_heapsize";
+    private static final String HEAPSIZE_PROP = "dalvik.vm.heapsize";
+    private static final String HEAPSIZE_PERSIST_PROP = "persist.sys.vm.heapsize";
+    private static final String HEAPSIZE_DEFAULT = "32m";
 
     private CheckBoxPreference mLockHomePref;
     private CheckBoxPreference mLockMmsPref;
+    private ListPreference mHeapsizePref;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,11 @@ public class PerformanceSettingsActivity extends PreferenceActivity {
         mLockMmsPref = (CheckBoxPreference) prefSet.findPreference(LOCK_MMS_PREF);
         mLockMmsPref.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.LOCK_MMS_IN_MEMORY, LOCK_MMS_DEFAULT) == 1);
+
+        mHeapsizePref = (ListPreference) prefSet.findPreference(HEAPSIZE_PREF);
+        mHeapsizePref.setValue(SystemProperties.get(HEAPSIZE_PERSIST_PROP, 
+                SystemProperties.get(HEAPSIZE_PROP, HEAPSIZE_DEFAULT)));
+        mHeapsizePref.setOnPreferenceChangeListener(this);
     }
     
     @Override
@@ -53,6 +65,16 @@ public class PerformanceSettingsActivity extends PreferenceActivity {
             return true;
         }
 
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeapsizePref) {
+            if (newValue != null) {
+                SystemProperties.set(HEAPSIZE_PERSIST_PROP, (String)newValue);
+            }
+        }
         return false;
     }
 
