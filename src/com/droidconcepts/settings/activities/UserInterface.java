d1041e89341bdcefe68847d9cfd24c18ea2d6b41
Woodyman <woodyman.dc@gmail.com>
2011-06-02 14:19:23 -0400

    adding Galaxy-S widget stuff, Ported to DC by Davros Credit: Pedlar

--- src/com/droidconcepts/settings/activities/UserInterface.java
+++ src/com/droidconcepts/settings/activities/UserInterface.java
@@ -8,6 +8,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 public class UserInterface extends PreferenceActivity {
 	
@@ -16,20 +17,24 @@ public class UserInterface extends PreferenceActivity {
     private static final String DATE_PROVIDER_SCREEN = "date_provider_settings";
     private static final String NOTIFICATION_SCREEN = "notification_settings";
     private static final String EXTRAS_SCREEN = "extra_tweaks";
+    private static final String ROTATION_90_PREF = "pref_rotation_90";
+    private static final String ROTATION_180_PREF = "pref_rotation_180";
+    private static final String ROTATION_270_PREF = "pref_rotation_270";
+    private static final String UI_EXP_WIDGET = "expanded_widget";
+    private static final String UI_EXP_WIDGET_COLOR = "expanded_color_mask";
+    private static final String UI_EXP_WIDGET_PICKER = "widget_picker";
 	
     private PreferenceScreen mBatteryClockScreen;
     private PreferenceScreen mDateProviderScreen;
     private PreferenceScreen mNotificationScreen;
     private PreferenceScreen mExtrasScreen;
-    
-    /* Other */	
-    private static final String ROTATION_90_PREF = "pref_rotation_90";
-    private static final String ROTATION_180_PREF = "pref_rotation_180";
-    private static final String ROTATION_270_PREF = "pref_rotation_270";
-    
     private CheckBoxPreference mRotation90Pref;
     private CheckBoxPreference mRotation180Pref;
     private CheckBoxPreference mRotation270Pref;
+    private CheckBoxPreference mPowerWidget;
+    private Preference mPowerWidgetColor;
+    private PreferenceScreen mPowerPicker;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,12 +60,16 @@ public class UserInterface extends PreferenceActivity {
         mRotation90Pref.setChecked((mode & 1) != 0);
         mRotation180Pref.setChecked((mode & 2) != 0);
         mRotation270Pref.setChecked((mode & 4) != 0);
+
+        /* Expanded View Power Widget */
+        mPowerWidget = (CheckBoxPreference) prefSet.findPreference(UI_EXP_WIDGET);
+        mPowerWidgetColor = prefSet.findPreference(UI_EXP_WIDGET_COLOR);
+        mPowerPicker = (PreferenceScreen)prefSet.findPreference(UI_EXP_WIDGET_PICKER);
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
         
-        /* Preference Screens */
         if (preference == mBatteryClockScreen) {
         	startActivity(mBatteryClockScreen.getIntent());
         }
@@ -73,7 +82,9 @@ public class UserInterface extends PreferenceActivity {
         if (preference == mExtrasScreen) {
         	startActivity(mExtrasScreen.getIntent());
         }
-        
+        if(preference == mPowerPicker) {
+            startActivity(mPowerPicker.getIntent());
+        }
         if (preference == mRotation90Pref ||
             preference == mRotation180Pref ||
             preference == mRotation270Pref) {
@@ -84,6 +95,35 @@ public class UserInterface extends PreferenceActivity {
             Settings.System.putInt(getContentResolver(),
                      Settings.System.ACCELEROMETER_ROTATION_MODE, mode);
         }
+        if(preference == mPowerWidget) {
+            value = mPowerWidget.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.EXPANDED_VIEW_WIDGET, value ? 1 : 0);
+        }
+
+        if (preference == mPowerWidgetColor) {
+            ColorPickerDialog cp = new ColorPickerDialog(this,
+                mWidgetColorListener,
+                readWidgetColor());
+            cp.show();
+        }
         return true;
     }
+
+    private int readWidgetColor() {
+        try {
+            return Settings.System.getInt(getContentResolver(), Settings.System.EXPANDED_VIEW_WIDGET_COLOR);
+        }
+        catch (SettingNotFoundException e) {
+            return -16777216;
+        }
+    }
+    ColorPickerDialog.OnColorChangedListener mWidgetColorListener =
+        new ColorPickerDialog.OnColorChangedListener() {
+            public void colorChanged(int color) {
+                Settings.System.putInt(getContentResolver(), Settings.System.EXPANDED_VIEW_WIDGET_COLOR, color);
+            }
+            public void colorUpdate(int color) {
+            }
+    };
 }
