1fd8c7ae5b35183f943a2d0a9fec6ab1aead11a4
Woodyman <woodyman.dc@gmail.com>
2011-06-05 23:43:50 -0400

    add option for go to sleep, and flashlight in the notificiation power widget, PEDLAR

--- src/com/droidconcepts/settings/activities/WidgetActivity.java
+++ src/com/droidconcepts/settings/activities/WidgetActivity.java
@@ -37,6 +37,8 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
     public static final String TOGGLE_NETWORKMODE_STR = "toggleNetworkMode";
     public static final String TOGGLE_AUTOROTATE_STR = "toggleAutoRotate";
     public static final String TOGGLE_AIRPLANE_STR = "toggleAirplane";
+    public static final String TOGGLE_FLASHLIGHT_STR = "toggleFlashlight";
+    public static final String TOGGLE_SLEEPMODE_STR = "toggleSleepMode";
 
     private static final String TOGGLE_WIFI = "toggle_wifi";
     private static final String TOGGLE_BLUETOOTH = "toggle_bluetooth";
@@ -55,6 +57,8 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
     private static final String EXP_NETWORK_MODE = "pref_network_mode";
     private static final String EXP_SCREENTIMEOUT_MODE = "pref_screentimeout_mode";
     private static final String EXP_RING_MODE = "pref_ring_mode";
+    private static final String TOGGLE_FLASHLIGHT = "toggle_flashlight";
+    private static final String TOGGLE_SLEEPMODE = "toggle_sleep";
 
     CheckBoxPreference mToggleWifi;
     CheckBoxPreference mToggleBluetooth;
@@ -69,10 +73,14 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
     CheckBoxPreference mToggleNetworkMode;
     CheckBoxPreference mToggleAutoRotate;
     CheckBoxPreference mToggleAirplane;
+    CheckBoxPreference mToggleFlashlight;
+    CheckBoxPreference mToggleSleepMode;
+
     ListPreference mBrightnessMode;
     ListPreference mNetworkMode;
     ListPreference mScreentimeoutMode;
     ListPreference mRingMode;
+
 
     private boolean isNull(String mString) {
         if (mString == null || mString.matches("null") || mString.length() == 0
@@ -149,6 +157,8 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
         mToggleNetworkMode = (CheckBoxPreference) prefSet.findPreference(TOGGLE_NETWORKMODE);
         mToggleAutoRotate = (CheckBoxPreference) prefSet.findPreference(TOGGLE_AUTOROTATE);
         mToggleAirplane = (CheckBoxPreference) prefSet.findPreference(TOGGLE_AIRPLANE);
+        mToggleFlashlight = (CheckBoxPreference) prefSet.findPreference(TOGGLE_FLASHLIGHT);
+        mToggleSleepMode = (CheckBoxPreference) prefSet.findPreference(TOGGLE_SLEEPMODE);
 
         mBrightnessMode = (ListPreference) prefSet.findPreference(EXP_BRIGHTNESS_MODE);
         mBrightnessMode.setOnPreferenceChangeListener(this);
@@ -158,6 +168,11 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
         mScreentimeoutMode.setOnPreferenceChangeListener(this);
         mRingMode = (ListPreference) prefSet.findPreference(EXP_RING_MODE);
         mRingMode.setOnPreferenceChangeListener(this);
+ 
+        if (!getResources().getBoolean(R.bool.has_led_flash)) {
+            mToggleFlashlight.setEnabled(false);
+            mFlashMode.setEnabled(false);
+        }
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -238,6 +253,18 @@ public class WidgetActivity extends PreferenceActivity implements OnPreferenceCh
             CheckBoxPreference keyPref = (CheckBoxPreference) preference;
             value = keyPref.isChecked();
             if(!manageList(TOGGLE_AIRPLANE_STR, value)) {
+                keyPref.setChecked(false);
+            }
+        }  else if (preference == mToggleFlashlight) {
+            CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+            value = keyPref.isChecked();
+            if(!manageList(TOGGLE_FLASHLIGHT_STR, value)) {
+                keyPref.setChecked(false);
+            }
+        }  else if (preference == mToggleSleepMode) {
+            CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+            value = keyPref.isChecked();
+            if(!manageList(TOGGLE_SLEEPMODE_STR, value)) {
                 keyPref.setChecked(false);
             }
         }
